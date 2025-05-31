1d6f6bb9bf99903544c573325e7b47b8f9eac1cd
Valeria Manriquez <vmanriquez@sourcing.cl>
2025-05-31 19:42:41 -0400

    change all

---
+++ valeriamanriquez/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="card">
+    <button type="button" @click="count++">click +</button>
+    <button type="button" @click="count--">click -</button>
+    <h3>{{ count }}</h3>
+  </div>
+</template>
